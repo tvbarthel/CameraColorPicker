449e2f5f8e97c9a04eb66f6ab9794f36e8868a65
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-12-07 16:18:38 +0100

    Add the camera preview mecanism

--- CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/MainActivity.java
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/MainActivity.java
@@ -1,19 +1,61 @@
 package fr.tvbarthel.apps.cameracolorpicker;
 
+import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import fr.tvbarthel.apps.cameracolorpicker.utils.Cameras;
+import fr.tvbarthel.apps.cameracolorpicker.views.CameraPreview;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    protected static final String TAG = MainActivity.class.getCanonicalName();
+
+    protected Camera mCamera;
+    protected boolean mIsPortrait;
+    protected FrameLayout mPreviewContainter;
+    protected FrameLayout.LayoutParams mPreviewParams;
+    protected CameraPreview mCameraPreview;
+    protected CameraAsyncTask mCameraAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIsPortrait = getResources().getBoolean(R.bool.is_portrait);
+        mPreviewContainter = (FrameLayout) findViewById(R.id.activity_main_preview_container);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Setup the camera asynchronously.
+        mCameraAsyncTask = new CameraAsyncTask();
+        mCameraAsyncTask.execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Cancel the Camera AsyncTask.
+        mCameraAsyncTask.cancel(true);
+
+        // Release the camera.
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +77,88 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A safe way to get an instance of the front camera
+     */
+    private static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        return c;
+    }
+
+    /**
+     * Async task used to configure and start camera
+     */
+    private class CameraAsyncTask extends AsyncTask<Void, Void, Camera> {
+
+        @Override
+        protected Camera doInBackground(Void... params) {
+            Camera camera = getCameraInstance();
+            if (camera == null) {
+                MainActivity.this.finish();
+            } else {
+                //configure Camera parameters
+                Camera.Parameters cameraParameters = camera.getParameters();
+
+                //get optimal camera preview size according to the layout used to display it
+                Camera.Size bestSize = Cameras.getBestPreviewSize(
+                        cameraParameters.getSupportedPreviewSizes()
+                        , mPreviewContainter.getWidth()
+                        , mPreviewContainter.getHeight()
+                        , mIsPortrait);
+                //set optimal camera preview
+                cameraParameters.setPreviewSize(bestSize.width, bestSize.height);
+                cameraParameters.setPictureSize(bestSize.width, bestSize.height);
+                camera.setParameters(cameraParameters);
+
+                //set camera orientation to match with current device orientation
+                Cameras.setCameraDisplayOrientation(MainActivity.this, camera);
+
+                //get proportional dimension for the layout used to display preview according to the preview size used
+                int[] adaptedDimension = Cameras.getProportionalDimension(
+                        bestSize
+                        , mPreviewContainter.getWidth()
+                        , mPreviewContainter.getHeight()
+                        , mIsPortrait);
+
+                //set up params for the layout used to display the preview
+                mPreviewParams = new FrameLayout.LayoutParams(adaptedDimension[0], adaptedDimension[1]);
+                mPreviewParams.gravity = Gravity.CENTER;
+            }
+            return camera;
+        }
+
+        @Override
+        protected void onPostExecute(Camera camera) {
+            super.onPostExecute(camera);
+
+            // Check if the task is cancelled before trying to use the camera.
+            if (!isCancelled()) {
+                mCamera = camera;
+                if (mCamera == null) {
+                    MainActivity.this.finish();
+                } else {
+                    //set up camera preview
+                    mCameraPreview = new CameraPreview(MainActivity.this, mCamera);
+
+                    //add camera preview
+                    mPreviewContainter.addView(mCameraPreview, 0, mPreviewParams);
+                }
+            }
+        }
+
+        @Override
+        protected void onCancelled(Camera camera) {
+            super.onCancelled(camera);
+            if (camera != null) {
+                camera.release();
+            }
+        }
     }
 }
---
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/utils/Cameras.java
@@ -0,0 +1,135 @@
+package fr.tvbarthel.apps.cameracolorpicker.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import java.util.List;
+
+/**
+ * Static utils methods for dealing with {@link android.hardware.Camera}.
+ */
+public final class Cameras {
+
+    private static final double ASPECT_TOLERANCE = 0.15;
+
+    /**
+     * Calculate the optimal size of camera preview according to layout used to display this preview
+     *
+     * @param sizes        Available camera preview sizes
+     * @param layoutWidth  width of layout used to display the preview
+     * @param layoutHeight height of the layout used to display the preview
+     * @param isPortrait   indicate if current orientation is portrait since the camera sizes are all in landscape mode
+     * @return best camera preview size which fit layout used to display the preview
+     */
+    public static Camera.Size getBestPreviewSize(List<Camera.Size> sizes, int layoutWidth, int layoutHeight, boolean isPortrait) {
+        if (isPortrait) {
+            // Inverse surfaceWidth and surfaceHeight since the sizes are all in landscape mode.
+            layoutHeight = layoutHeight + layoutWidth;
+            layoutWidth = layoutHeight - layoutWidth;
+            layoutHeight = layoutHeight - layoutWidth;
+        }
+        double targetRatio = (double) layoutWidth / layoutHeight;
+        Camera.Size optimalSize = null;
+        double optimalArea = 0;
+
+        // Try to find the size that matches the target ratio and has the max area.
+        for (Camera.Size candidateSize : sizes) {
+            double candidateRatio = (double) candidateSize.width / candidateSize.height;
+            double candidateArea = candidateSize.width * candidateSize.height;
+            double ratioDifference = Math.abs(candidateRatio - targetRatio);
+            if (ratioDifference < ASPECT_TOLERANCE && candidateArea > optimalArea) {
+                optimalSize = candidateSize;
+                optimalArea = candidateArea;
+            }
+        }
+
+        // Cannot find a size that matches the target ratio.
+        // Try to find the size that has the max area.
+        if (optimalSize == null) {
+            optimalArea = 0;
+            for (Camera.Size candidateSize : sizes) {
+                double candidateArea = candidateSize.width * candidateSize.height;
+                if (candidateArea > optimalArea) {
+                    optimalSize = candidateSize;
+                    optimalArea = candidateArea;
+                }
+            }
+        }
+
+        return optimalSize;
+    }
+
+    /**
+     * calculate proportional layout dimension for displaying a camera preview according to a given camera preview size
+     *
+     * @param size       Camera preview size used {@see CameraUtils.getBestPreviewSize}
+     * @param targetW    current width of the layout which will host camera preview
+     * @param targetH    current height of the layout which will host camera preview
+     * @param isPortrait indicate if current orientation is portrait since the camera sizes are all in landscape mode
+     * @return dimension which matches camera preview size ratio
+     */
+    public static int[] getProportionalDimension(Camera.Size size, int targetW, int targetH, boolean isPortrait) {
+        int[] adaptedDimension = new int[2];
+        double previewRatio;
+
+        if (isPortrait) {
+            previewRatio = (double) size.height / size.width;
+        } else {
+            previewRatio = (double) size.width / size.height;
+        }
+
+        if (((double) targetW / targetH) > previewRatio) {
+            adaptedDimension[0] = targetW;
+            adaptedDimension[1] = (int) (adaptedDimension[0] / previewRatio);
+        } else {
+            adaptedDimension[1] = targetH;
+            adaptedDimension[0] = (int) (adaptedDimension[1] * previewRatio);
+        }
+
+        return adaptedDimension;
+    }
+
+    /**
+     * used to adapt camera preview to the current device orientation
+     *
+     * @param camera
+     */
+    public static void setCameraDisplayOrientation(Context context, android.hardware.Camera camera) {
+        final Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, info);
+        int degrees = 0;
+        final int currentRotation = ((WindowManager) context.getSystemService(Activity.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+        switch (currentRotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int displayOrientation = 0;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            displayOrientation = (info.orientation + degrees) % 360;
+            displayOrientation = (360 - displayOrientation) % 360;  // compensate the mirror
+        } else {  // back-facing
+            displayOrientation = (info.orientation - degrees + 360) % 360;
+        }
+
+        camera.setDisplayOrientation(displayOrientation);
+    }
+
+
+    //Non instantiable class.
+    private Cameras() {
+    }
+}
