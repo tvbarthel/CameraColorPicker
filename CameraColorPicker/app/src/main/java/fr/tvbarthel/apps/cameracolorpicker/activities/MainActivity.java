90b175744d9fd487678d88e768b5cf8c5265e26a
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-01-18 13:08:49 +0100

    Add a list view to display future colors

--- CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/activities/MainActivity.java
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/activities/MainActivity.java
@@ -1,9 +1,16 @@
 package fr.tvbarthel.apps.cameracolorpicker.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.tvbarthel.apps.cameracolorpicker.R;
+import fr.tvbarthel.apps.cameracolorpicker.adapters.ColorAdapter;
 
 /**
  * TODO comment
@@ -14,5 +21,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ColorAdapter colorAdapter = new ColorAdapter(this, fakeColors());
+        final View emptyView = findViewById(R.id.activity_main_empty_view);
+        final ListView colorList = (ListView) findViewById(R.id.activity_main_list_view);
+        colorList.setAdapter(colorAdapter);
+        colorList.setEmptyView(emptyView);
+    }
+
+    // TODO remove (for test purpose only)
+    private List<Integer> fakeColors() {
+        final List<Integer> fakeColors = new ArrayList<>(11);
+        fakeColors.add(Color.BLACK);
+        fakeColors.add(Color.BLUE);
+        fakeColors.add(Color.CYAN);
+        fakeColors.add(Color.DKGRAY);
+        fakeColors.add(Color.GRAY);
+        fakeColors.add(Color.GREEN);
+        fakeColors.add(Color.LTGRAY);
+        fakeColors.add(Color.MAGENTA);
+        fakeColors.add(Color.RED);
+        fakeColors.add(Color.WHITE);
+        fakeColors.add(Color.YELLOW);
+        return fakeColors;
     }
 }
---
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/adapters/ColorAdapter.java
@@ -0,0 +1,60 @@
+package fr.tvbarthel.apps.cameracolorpicker.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fr.tvbarthel.apps.cameracolorpicker.R;
+
+/**
+ * TODO comment.
+ */
+public class ColorAdapter extends ArrayAdapter<Integer> {
+
+    public ColorAdapter(Context context, List<Integer> objects) {
+        super(context, R.layout.row_color_item, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (view == null) {
+            view = createView(parent);
+        }
+
+        final ViewHolder viewHolder = (ViewHolder) view.getTag();
+        bindViewHolder(viewHolder, position);
+
+        return view;
+    }
+
+    protected View createView(ViewGroup parent) {
+        final View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_color_item, parent, false);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+        return view;
+    }
+
+    protected void bindViewHolder(ViewHolder viewHolder, int position) {
+        final int color = getItem(position);
+        viewHolder.mColorPreview.setBackgroundColor(color);
+        viewHolder.mColorText.setText(Integer.toHexString(color));
+    }
+
+    private static class ViewHolder {
+        public View mColorPreview;
+        public TextView mColorText;
+
+        public ViewHolder(View view) {
+            mColorPreview = view.findViewById(R.id.row_color_item_preview);
+            mColorText = (TextView) view.findViewById(R.id.row_color_item_text);
+        }
+    }
+}
