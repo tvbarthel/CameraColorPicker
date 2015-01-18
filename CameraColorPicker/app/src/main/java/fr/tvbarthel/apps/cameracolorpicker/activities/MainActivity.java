d6bd97b517351e5491c2d1af181bb97b62807b1c
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-01-18 12:22:23 +0100

    Add MainActivity

---
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package fr.tvbarthel.apps.cameracolorpicker.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import fr.tvbarthel.apps.cameracolorpicker.R;
+
+/**
+ * TODO comment
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
