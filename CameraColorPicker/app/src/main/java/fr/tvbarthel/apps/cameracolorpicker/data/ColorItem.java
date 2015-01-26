47dc144c17a2259dbcdea5237c77d0c755debfad
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-01-26 23:34:34 +0100

    Comment the ColorItem class

--- CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/data/ColorItem.java
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/data/ColorItem.java
@@ -1,38 +1,63 @@
 package fr.tvbarthel.apps.cameracolorpicker.data;
 
 /**
- * TODO comment.
+ * A simple data class representing a color.
  */
 public class ColorItem {
 
+    /**
+     * The id of the color.
+     */
     protected final long mId;
-    protected int mColor;
-    protected long mCreationTime;
 
+    /**
+     * An int representing the value of the color.
+     */
+    protected final int mColor;
 
+    /**
+     * A long representing the creation time of the color. (In milliseconds).
+     */
+    protected final long mCreationTime;
+
+    /**
+     * Create a new {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem} with an id and a color.
+     *
+     * @param id    the id of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}. Should be unique.
+     * @param color the color value.
+     */
     public ColorItem(long id, int color) {
         mId = id;
         mColor = color;
         mCreationTime = System.currentTimeMillis();
     }
 
+    /**
+     * Get the id of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     *
+     * @return the id of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     */
     public long getId() {
         return mId;
     }
 
+    /**
+     * Get the value of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     *
+     * @return an integer representing the color value of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     */
     public int getColor() {
         return mColor;
     }
 
-    public void setColor(int color) {
-        mColor = color;
-    }
 
+    /**
+     * Get the creation time of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     *
+     * @return a long representing the creation time in milliseconds.
+     */
     public long getCreationTime() {
         return mCreationTime;
     }
 
-    public void setCreationTime(long creationTime) {
-        mCreationTime = creationTime;
-    }
 }
