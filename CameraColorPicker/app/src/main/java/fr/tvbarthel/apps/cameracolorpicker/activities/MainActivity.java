2369a644b087412f230445afce133b715a18ca99
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-01-28 22:31:52 +0100

    Copy the hexadecimal value on ColorItem long click

--- CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/activities/MainActivity.java
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/activities/MainActivity.java
@@ -1,15 +1,21 @@
 package fr.tvbarthel.apps.cameracolorpicker.activities;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.melnykov.fab.FloatingActionButton;
 
 import fr.tvbarthel.apps.cameracolorpicker.R;
 import fr.tvbarthel.apps.cameracolorpicker.adapters.ColorAdapter;
+import fr.tvbarthel.apps.cameracolorpicker.data.ColorItem;
 import fr.tvbarthel.apps.cameracolorpicker.data.ColorItems;
 
 /**
@@ -19,16 +25,34 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     protected ColorAdapter mColorAdapter;
 
+    /**
+     * The user-visible label for the clip {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     */
+    protected String mClipColorItemLabel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mClipColorItemLabel = getString(R.string.activity_main_clip_color_item_label);
 
         mColorAdapter = new ColorAdapter(this);
         final View emptyView = findViewById(R.id.activity_main_empty_view);
         final ListView colorList = (ListView) findViewById(R.id.activity_main_list_view);
         colorList.setAdapter(mColorAdapter);
         colorList.setEmptyView(emptyView);
+        colorList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final ColorItem colorItem = mColorAdapter.getItem(position);
+                final ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                final ClipData clip = ClipData.newPlainText(mClipColorItemLabel, colorItem.getHexString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(view.getContext(), R.string.activity_main_color_success_copy_message, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.activity_main_fab);
         fab.attachToListView(colorList);
