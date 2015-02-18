6ed454440a256dde939638a6126509677456ce31
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-02-18 08:34:41 +0100

    Comment the ColorItems class.

--- CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/data/ColorItems.java
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/data/ColorItems.java
@@ -17,18 +17,38 @@ import java.util.List;
 
 /**
  * Static methods for dealing with {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s.
- * <p/>
- * TODO comment.
  */
 public final class ColorItems {
 
-    private static final String KEY_SAVED_COLOR_ITEMS = "Colors.Keys.SAVED_COLOR_COLOR_ITEMS";
+    /**
+     * A key for saving the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s of the user.
+     */
+    private static final String KEY_SAVED_COLOR_ITEMS = "Colors.Keys.SAVED_COLOR_ITEMS";
+
+    /**
+     * A key for saving the last picked color.
+     */
     private static final String KEY_LAST_PICKED_COLOR = "Colors.Keys.LAST_PICKED_COLOR";
+
+    /**
+     * The default last picked color.
+     */
     private static final int DEFAULT_LAST_PICKED_COLOR = Color.WHITE;
+
+    /**
+     * A {@link com.google.gson.Gson} for serializing/deserializing objects.
+     */
     private static final Gson GSON = new Gson();
+
+    /**
+     * A {@link java.lang.reflect.Type} instance of a {@link java.util.List} of {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s.
+     */
     private static final Type COLOR_ITEM_LIST_TYPE = new TypeToken<List<ColorItem>>() {
     }.getType();
 
+    /**
+     * A {@link java.util.Comparator} for sorting {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s in chronological order of creation.
+     */
     public static final Comparator<ColorItem> CHRONOLOGICAL_COMPARATOR = new Comparator<ColorItem>() {
         @Override
         public int compare(ColorItem lhs, ColorItem rhs) {
@@ -36,24 +56,55 @@ public final class ColorItems {
         }
     };
 
+    /**
+     * Get the {@link android.content.SharedPreferences} used for saving/restoring data.
+     *
+     * @param context a {@link android.content.Context}.
+     * @return the {@link android.content.SharedPreferences} used for saving/restoring data.
+     */
     private static SharedPreferences getPreferences(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
 
+    /**
+     * Get the last picked color.
+     *
+     * @param context a {@link android.content.Context}.
+     * @return the last picked color.
+     */
     public static int getLastPickedColor(Context context) {
         return getPreferences(context).getInt(KEY_LAST_PICKED_COLOR, DEFAULT_LAST_PICKED_COLOR);
     }
 
+    /**
+     * Save the last picked color.
+     *
+     * @param context         a {@link android.content.Context}.
+     * @param lastPickedColor the last picked color.
+     * @return Returns true if the new color was successfully written to persistent storage.
+     */
     public static boolean saveLastPickedColor(Context context, int lastPickedColor) {
         final SharedPreferences.Editor editor = getPreferences(context).edit();
         editor.putInt(KEY_LAST_PICKED_COLOR, lastPickedColor);
         return editor.commit();
     }
 
+    /**
+     * Get the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s of the user.
+     *
+     * @param context a {@link android.content.Context}.
+     * @return a {@link java.util.List} of {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s.
+     */
     public static List<ColorItem> getSavedColorItems(Context context) {
         return getSavedColorItems(getPreferences(context));
     }
 
+    /**
+     * Get the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s of the user.
+     *
+     * @param sharedPreferences a {@link android.content.SharedPreferences} from which the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s will be retrieved.
+     * @return a {@link java.util.List} of {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s.
+     */
     @SuppressWarnings("unchecked")
     public static List<ColorItem> getSavedColorItems(SharedPreferences sharedPreferences) {
         final String jsonColorItems = sharedPreferences.getString(KEY_SAVED_COLOR_ITEMS, "");
@@ -72,6 +123,13 @@ public final class ColorItems {
         return colorItems;
     }
 
+    /**
+     * Save a {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s.
+     *
+     * @param context     a {@link android.content.Context}.
+     * @param colorToSave the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem} to save.
+     * @return Returns true if the new {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem} was successfully written to persistent storage.
+     */
     public static boolean saveColorItem(Context context, ColorItem colorToSave) {
         if (colorToSave == null) {
             throw new IllegalArgumentException("Can't save a null color.");
@@ -88,6 +146,13 @@ public final class ColorItems {
         return editor.commit();
     }
 
+    /**
+     * Delete a {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}.
+     *
+     * @param context           a {@link android.content.Context}.
+     * @param colorItemToDelete the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem} to be deleted.
+     * @return Returns true if the new color was successfully deleted from persistent storage.
+     */
     public static boolean deleteColorItem(Context context, ColorItem colorItemToDelete) {
         if (colorItemToDelete == null) {
             throw new IllegalArgumentException("Can't delete a null color item");
@@ -109,15 +174,33 @@ public final class ColorItems {
         return false;
     }
 
+    /**
+     * Register a {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItems.OnColorItemChangeListener}.
+     * <p/>
+     * <b>Caution</b>: No strong references to the listener are currently stored. You must store a strong reference to the listener, or it will be susceptible to garbage collection.
+     *
+     * @param context                   a {@link android.content.Context}.
+     * @param onColorItemChangeListener the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItems.OnColorItemChangeListener} to register.
+     */
     public static void registerListener(Context context, OnColorItemChangeListener onColorItemChangeListener) {
         getPreferences(context).registerOnSharedPreferenceChangeListener(onColorItemChangeListener);
     }
 
+    /**
+     * Unregister a {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItems.OnColorItemChangeListener}.
+     *
+     * @param context                   a {@link android.content.Context}.
+     * @param onColorItemChangeListener the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItems.OnColorItemChangeListener} to unregister.
+     */
     public static void unregisterListener(Context context, OnColorItemChangeListener onColorItemChangeListener) {
         getPreferences(context).unregisterOnSharedPreferenceChangeListener(onColorItemChangeListener);
     }
 
+    /**
+     * A simple class for listening to the changes of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s of the user.
+     */
     public abstract static class OnColorItemChangeListener implements SharedPreferences.OnSharedPreferenceChangeListener {
+
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
             if (KEY_SAVED_COLOR_ITEMS.equals(key)) {
@@ -125,6 +208,11 @@ public final class ColorItems {
             }
         }
 
+        /**
+         * Called when the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s of the user have just changed.
+         *
+         * @param colorItems the current {@link java.util.List} of the {@link fr.tvbarthel.apps.cameracolorpicker.data.ColorItem}s of the user.
+         */
         public abstract void onColorItemChanged(List<ColorItem> colorItems);
     }
 
