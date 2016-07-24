aacfc8c2cf254ba6e51dbe19f90c88891683414f
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2016-07-24 15:04:48 +0200

    [Palette list] Adapt the palette list to the kids/adult falvors.
    The PaletteListWrapper describes the common contract of the wrapper.
    The FlavorPaletteListWrapper defines the specific behaviors.
    
    For the kids flavor, the palettes are presented in a grid.
    Only a preview of the palette is presented.
    
    For the adult flavor, the palettes are presented in a vertical list.
    The name of the palette is displayed next to the preview.

--- CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/views/PaletteListPage.java
+++ CameraColorPicker/app/src/main/java/fr/tvbarthel/apps/cameracolorpicker/views/PaletteListPage.java
@@ -3,25 +3,25 @@ package fr.tvbarthel.apps.cameracolorpicker.views;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.FrameLayout;
-import android.widget.ListView;
 
 import java.util.List;
 
 import fr.tvbarthel.apps.cameracolorpicker.R;
 import fr.tvbarthel.apps.cameracolorpicker.activities.PaletteDetailActivity;
-import fr.tvbarthel.apps.cameracolorpicker.adapters.PaletteAdapter;
 import fr.tvbarthel.apps.cameracolorpicker.data.Palette;
 import fr.tvbarthel.apps.cameracolorpicker.data.Palettes;
+import fr.tvbarthel.apps.cameracolorpicker.wrappers.PaletteListWrapper;
 
 /**
  * A simple {@link FrameLayout} used in the {@link android.support.v4.view.ViewPager} of the {@link fr.tvbarthel.apps.cameracolorpicker.activities.MainActivity}
  * to display the list of the {@link Palette}s that the user created.
  */
-public class PaletteListPage extends FrameLayout {
+public class PaletteListPage extends FrameLayout implements PaletteListWrapper.PaletteListWrapperListener {
 
     /**
      * An {@link Palettes.OnPaletteChangeListener} that will be notified when the palettes of the user change.
@@ -60,29 +60,22 @@ public class PaletteListPage extends FrameLayout {
         final View view = View.inflate(context, R.layout.view_palette_list_page, this);
 
         final View emptyView = view.findViewById(R.id.view_palette_list_empty_view);
-        final ListView listView = (ListView) view.findViewById(R.id.view_palette_list_page_list_view);
-        listView.setEmptyView(emptyView);
-
-        final PaletteAdapter adapter = new PaletteAdapter(context);
-        adapter.addAll(Palettes.getSavedColorPalettes(context));
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.view_palette_list_page_list_view);
+        final FlavorPaletteListWrapper wrapper = FlavorPaletteListWrapper.create(recyclerView, this);
+        final PaletteListWrapper.Adapter adapter = wrapper.installRecyclerView();
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final Palette palette = adapter.getItem(position);
-                PaletteDetailActivity.startWithColorPalette(view.getContext(), palette,
-                        view.findViewById(R.id.row_color_palette_thumbnail));
+            public void onChanged() {
+                super.onChanged();
+                emptyView.setVisibility(adapter.getItemCount() == 0 ? VISIBLE : GONE);
             }
         });
 
+        adapter.setPalettes(Palettes.getSavedColorPalettes(context));
         mOnPaletteChangeListener = new Palettes.OnPaletteChangeListener() {
             @Override
             public void onColorPaletteChanged(List<Palette> palettes) {
-                adapter.clear();
-                adapter.addAll(palettes);
-                adapter.notifyDataSetChanged();
+                adapter.setPalettes(palettes);
             }
         };
     }
@@ -101,5 +94,10 @@ public class PaletteListPage extends FrameLayout {
         // We do not need to be notified anymore.
         Palettes.unregisterListener(getContext(), mOnPaletteChangeListener);
         super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onPaletteClicked(@NonNull Palette palette, @NonNull View palettePreview) {
+        PaletteDetailActivity.startWithColorPalette(getContext(), palette, palettePreview);
     }
 }
